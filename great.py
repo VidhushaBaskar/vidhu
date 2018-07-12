3f13bbd161cb74e1b1661b656fe015ed727ee5c3
VidhushaBaskar <41140126+VidhushaBaskar@users.noreply.github.com>
2018-07-12 15:10:02 +0530

    Update great.py

--- great.py
+++ great.py
@@ -1,6 +1,10 @@
-a=int(input(""))
-b=int(input(""))
-c=int(input(""))
+import sys
+S = sys.stdin.read()
+S = S.split()
+S = [int(i) for i in S]
+a = S[0]
+b = S[1]
+c = S[2]
 if a>b and a>c :
 	print(a)
 elif(b>c):
