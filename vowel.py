c071783bafd146a0d7437c88f4fbd8e509009602
VidhushaBaskar <41140126+VidhushaBaskar@users.noreply.github.com>
2018-07-12 14:13:11 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+s=input(" ")
+l=['a','e','i','o','u','A','E','I','O','U']
+if(((s>='a')and(s<='z'))or ((s>='A')and(s<='Z'))):
+	if s in l:
+		print("Vowel")
+	else:
+		print("Consonant")
+else:
+	print("invalid")
