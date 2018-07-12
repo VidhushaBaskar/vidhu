db8f1f70969507d68a56009ad9d495827eb3bd9c
VidhushaBaskar <41140126+VidhushaBaskar@users.noreply.github.com>
2018-07-12 14:28:26 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
 s=input("")
 l=['a','e','i','o','u','A','E','I','O','U']
-if(((s>='a')and(s<='z'))or ((s>='A')and(s<='Z'))):
+if((s>='a'and s<='z') or (s>='A'and s<='Z')):
 	if s in l:
 		print("Vowel")
 	else:
